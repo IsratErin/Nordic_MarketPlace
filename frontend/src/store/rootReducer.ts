34383dd75a71c7f4b78664947194faf6dc585c71
IsratErin <israterin11@gmail.com>
2025-12-18 15:21:31 +0100

    initial Redux , store, routing setup

---
+++ frontend/src/store/rootReducer.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+// Feature reducers
+const rootReducer = combineReducers({
+  // auth: authReducer,
+  // products: productReducer,
+  // cart: cartReducer,
+});
+
+export default rootReducer;
