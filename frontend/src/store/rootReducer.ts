b8052a9fd6b04b20f19efff83ef2d731ad9a885c
IsratErin <israterin11@gmail.com>
2025-12-19 11:19:33 +0100

    app routing and productSlice with update product Info action

--- frontend/src/store/rootReducer.ts
+++ frontend/src/store/rootReducer.ts
@@ -1,10 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import productReducer from "../features/products/store/productSlice";
 
 // Feature reducers
 const rootReducer = combineReducers({
-  // auth: authReducer,
-  // products: productReducer,
-  // cart: cartReducer,
+  products: productReducer,
 });
 
 export default rootReducer;
